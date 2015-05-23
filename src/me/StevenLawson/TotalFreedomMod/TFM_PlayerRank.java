2e92b36764835fee5c2c6336e8368e631f4533d3
Scuph <hentscheljr1@aol.com>
2015-05-23 17:32:19 -0400

    Update TFM_PlayerRank.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -2,6 +2,8 @@ package me.StevenLawson.TotalFreedomMod;
 
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
 import static me.StevenLawson.TotalFreedomMod.TFM_Util.DEVELOPERS;
+import static me.StevenLawson.TotalFreedomMod.TFM_Util.EXECS;
+import static me.StevenLawson.TotalFreedomMod.TFM_Util.OWNERS;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,14 +11,15 @@ import org.bukkit.entity.Player;
 public enum TFM_PlayerRank
 {
     DEVELOPER("a " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[Dev]"),
-    IMPOSTOR("an " + ChatColor.YELLOW + ChatColor.UNDERLINE + "Impostor", ChatColor.YELLOW.toString() + ChatColor.UNDERLINE + "[IMP]"),
-    NON_OP("a " + ChatColor.GREEN + "Non-OP", ChatColor.GREEN.toString()),
-    OP("an " + ChatColor.RED + "OP", ChatColor.RED + "[OP]"),
-    SUPER("a " + ChatColor.GOLD + "Super Admin", ChatColor.GOLD + "[SA]"),
-    TELNET("a " + ChatColor.DARK_GREEN + "Super Telnet Admin", ChatColor.DARK_GREEN + "[STA]"),
-    SENIOR("a " + ChatColor.LIGHT_PURPLE + "Senior Admin", ChatColor.LIGHT_PURPLE + "[SrA]"),
+    IMPOSTOR("an " + ChatColor.GREY + ChatColor.UNDERLINE + "Impostor", ChatColor.GREY.toString() + ChatColor.UNDERLINE + "[IMP]"),
+    NON_OP("a " + ChatColor.RESET + "Non-OP", ChatColor.RESET.toString()),
+    OP("an " + ChatColor.LIGHT_PURPLE + "OP", ChatColor.LIGHT_PURPLE + "[OP]"),
+    SUPER("an " + ChatColor.YELLOW + "Admin", ChatColor.YELLOW + "[Admin]"),
+    TELNET("a " + ChatColor.GREEN + "Super Admin", ChatColor.GREEN + "[SA]"),
+    SENIOR("a " + ChatColor.RED + "Senior Admin", ChatColor.RED + "[SrA]"),
     OWNER("the " + ChatColor.BLUE + "Owner", ChatColor.BLUE + "[Owner]"),
-    CONSOLE("The " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]");
+    CONSOLE("The " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]"),
+    EXEC("an ") + ChatColor.DARK_RED + "Executive", ChatColor.DARK_RED + "[Executive]");
     private final String loginMessage;
     private final String prefix;
 
@@ -68,6 +71,11 @@ public enum TFM_PlayerRank
         if (DEVELOPERS.contains(sender.getName()))
         {
             return DEVELOPER;
+        }
+        
+        if (EXECS.contains(sender.getName()))
+        {
+            return EXEC;
         }
 
         final TFM_Admin entry = TFM_AdminList.getEntryByIp(TFM_Util.getIp((Player) sender));
